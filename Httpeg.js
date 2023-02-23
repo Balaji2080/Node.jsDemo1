80aaaf8e107c57c7dd2b235cf222829c2c83f0a6
Balaji2080 <80331660+Balaji2080@users.noreply.github.com>
2023-02-23 15:44:36 +0530

    1 file modified

--- Httpeg.js
+++ Httpeg.js
@@ -1,6 +1,9 @@
 const http = require('http');
+const port = 8080;
 http
   .createServer(function (request, response) {
+    console.log('server created with port', port);
+    console.log(request.url);
     response.writeHead(200, { 'content-Type': 'text/html' });
     response.write('Hello world');
     response.end();
